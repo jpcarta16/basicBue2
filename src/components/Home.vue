c64420a4cd09a8fee29c4899d6e19c1f40f3f32c
juan <jpcarta16@gmail.com>
2023-06-21 10:31:22 +0200

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Welcome to the Home Page!</h1>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage', 
+  data() {
+    return {
+      message: 'This is the home page of our application.'
+    };
+  }
+};
+</script>
---
+++ src/components/User.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>User Profile</h1>
+    <p>Name: {{ user.name }}</p>
+    <p>Email: {{ user.email }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserProfile',
+  data() {
+    return {
+      user: null
+    };
+  },
+  mounted() {
+    this.fetchUser();
+  },
+  methods: {
+    fetchUser() {
+      setTimeout(() => {
+        this.user = {
+          name: 'John Doe',
+          email: 'johndoe@example.com'
+        };
+      }, 1000);
+    }
+  }
+};
+</script>
